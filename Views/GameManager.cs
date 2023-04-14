63d34ed8f9607ba5506c0b245b7f1da6cd19c377
Malakh <jsbrown7@gmail.com>
2023-04-14 12:24:10 -0700

    changed parser to use switch/case vs if/else if

--- Views/GameManager.cs
+++ Views/GameManager.cs
@@ -42,171 +42,125 @@ namespace CSIFEngine
 
         public void Parse(string[] words)
         {
-            string command = words[0];
+            string command = words[0].ToLower();
 
             bool isTrig = false;
 
-            // Is this a signle word command?
             if (words.Length == 1)
             {
-                // Check if it is a custom name exitTrig
                 foreach (Exit exit in player.Location.ExitList)
                 {
-                    if (exit.exitTrig != null)
+                    if (exit.exitTrig != null && command == exit.exitTrig.ToLower())
                     {
-                        if (command.ToLower() == exit.exitTrig.ToLower())
-                        {
-                            isTrig = true;
-                            //Console.WriteLine("triggered");
-                        }
+                        isTrig = true;
                     }
-                }  // Now check direction exits or if isTrig was set true above.
-                if (command.ToLower() == "n" || command.ToLower() == "s" || command.ToLower() == "e" || command.ToLower() == "w" ||
-                    command.ToLower() == "nw" || command.ToLower() == "ne" || command.ToLower() == "sw" || command.ToLower() == "se" || isTrig)
+                }
+
+                if (new string[] { "n", "s", "e", "w", "nw", "ne", "sw", "se" }.Contains(command) || isTrig)
                 {
-                    // Add go the dir or exitTrig
                     words = new string[2] { "go", command };
-
-                    command = words[0];
-                }
-                
-            }
-
-            if (command.ToLower() == "look" || command.ToLower() == "l")
-            {
-                if (words.Length > 1)
-                { 
-                    player.Look(words[1].ToLower());
-                    player.PlayerTurns++;
-                }
-                else
-                {
-                    player.Look("room");
-                    player.PlayerTurns++;
+                    command = words[0].ToLower();
                 }
             }
-            else if (command.ToLower() == "go")
-            {
-                if (words.Length > 1)
-                {
-                    string arg1 = words[1];
 
-                    player.Go(arg1.ToLower());
-                    player.PlayerTurns++;
-                }
-            }
-            else if (command.ToLower() == "get")
+            switch (command)
             {
-                if (words.Length == 2)
-                {
-                    string arg1 = words[1];
-                    
-                    player.Get(arg1.ToLower());
+                case "look":
+                case "l":
+                    player.Look(words.Length > 1 ? words[1].ToLower() : "room");
                     player.PlayerTurns++;
-                }
-                else if (words.Length > 2)
-                {
-                    if (words.Length == 3)
+                    break;
+
+                case "go":
+                    if (words.Length > 1)
                     {
-                        string arg1 = words[1];
-                        string arg2 = words[2];
-
-                        player.Get(arg1.ToLower(), arg2);
+                        player.Go(words[1].ToLower());
                         player.PlayerTurns++;
                     }
-                    else if (words.Length == 4)
-                    {
-                        string arg1 = words[1];
-                        string arg2 = words[2];
-                        string arg3 = words[3];
+                    break;
 
-                        player.Get(arg1.ToLower(), arg3);
+                case "get":
+                case "take":
+                case "pickup":
+                    if (words.Length == 2)
+                    {
+                        player.Get(words[1].ToLower());
                         player.PlayerTurns++;
                     }
-                }
-            }
-            else if (command.ToLower() == "drop")
-            {
-                if (words.Length > 1)
-                {
-                    string arg1 = words[1];
-
-                    player.Drop(arg1.ToLower());
-                    player.PlayerTurns++;
-                }
-            }
-            else if (command.ToLower() == "inventory" || command.ToLower() == "inv" || command.ToLower() == "i")
-            {
-                player.ShowInv();
-                player.PlayerTurns++;
-            }
-            else if (command.ToLower() == "lock")
-            {
-                if (words.Length > 1)
-                {
-                    string arg1 = words[1];
-
-                    player.Lock(arg1.ToLower());
-                    player.PlayerTurns++;
-                }
-            }
-            else if (command.ToLower() == "unlock")
-            {
-
-                if (words.Length > 1)
-                {
-                    string arg1 = words[1];
-
-                    player.Unlock(arg1.ToLower());
-                    player.PlayerTurns++;
-                }
-       
-            }
-            else if (command.ToLower() == "open")
-            {
-                if (words.Length > 1)
-                {
-                    string arg1 = words[1];
-
-                    player.Open(arg1.ToLower());
-                    player.PlayerTurns++;
-                }
-            }
-            else if (command.ToLower() == "wear" || command.ToLower() == "equip")
-            {
-                if (words.Length > 1)
-                {
-                    string arg1 = words[1];
-
-                    player.Equip(arg1.ToLower());
-                    player.PlayerTurns++;
-                }
-            }
-            else if (command.ToLower() == "info")
-            {
-                player.Info();
-            }
-
-
-            else if (command.ToLower() == "quit")
-            {
-                Game.GameOver = true;
-            }
-            else
-            {
-                // Check if any items in the player inventory have the listener flag set true
-                // If they do then pass the commands to it for parsing  (not fully tested and/or implemented).
-                foreach (Thing item in player.Inventory)
-                {
-                    if (item != null )
+                    else if (words.Length >= 3)
                     {
-                        if (item.Listener)
+                        player.Get(words[1].ToLower(), words[words.Length == 3 ? 2 : 3]);
+                        player.PlayerTurns++;
+                    }
+                    break;
+
+                case "drop":
+                    if (words.Length > 1)
+                    {
+                        player.Drop(words[1].ToLower());
+                        player.PlayerTurns++;
+                    }
+                    break;
+
+                case "inventory":
+                case "inv":
+                case "i":
+                    player.ShowInv();
+                    player.PlayerTurns++;
+                    break;
+
+                case "lock":
+                    if (words.Length > 1)
+                    {
+                        player.Lock(words[1].ToLower());
+                        player.PlayerTurns++;
+                    }
+                    break;
+
+                case "unlock":
+                    if (words.Length > 1)
+                    {
+                        player.Unlock(words[1].ToLower());
+                        player.PlayerTurns++;
+                    }
+                    break;
+
+                case "open":
+                    if (words.Length > 1)
+                    {
+                        player.Open(words[1].ToLower());
+                        player.PlayerTurns++;
+                    }
+                    break;
+
+                case "wear":
+                case "equip":
+                    if (words.Length > 1)
+                    {
+                        player.Equip(words[1].ToLower());
+                        player.PlayerTurns++;
+                    }
+                    break;
+
+                case "info":
+                    player.Info();
+                    break;
+
+                case "quit":
+                    Game.GameOver = true;
+                    break;
+
+                default:
+                    foreach (Thing item in player.Inventory)
+                    {
+                        if (item != null && item.Listener)
                         {
                             item.Parse(words);
                         }
                     }
-                }
+                    break;
             }
         }
+
     }
 }
