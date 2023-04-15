ab8ed27157366218247209316c8f655154d0e1fb
Malakh <jsbrown7@gmail.com>
2023-04-15 16:15:42 -0700

    added GameState class for future Save/Load methods.

---
+++ Models/GameState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSIFEngine
+{
+    [Serializable]
+    public class GameState
+    {
+        public List<Room> Rooms { get; set; }
+        public List<Thing> Things { get; set; }
+        public Player Player { get; set; }
+
+        public GameState(List<Room> rooms, List<Thing> things, Player player)
+        {
+            Rooms = rooms;
+            Things = things;
+            Player = player;
+        }
+    }
+}
